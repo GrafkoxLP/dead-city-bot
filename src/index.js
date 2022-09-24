b946bc9c00169cf90537bcc3a8177697e4865dce
Leon Herschel <80112639+GrafkoxLP@users.noreply.github.com>
2022-09-25 00:07:00 +0200

    added packages

--- src/index.js
+++ src/index.js
@@ -49,21 +49,41 @@ client.on("interactionCreate", async (interaction) => {
         }
     }
 })
+const statusArray = [
+    {
+        name: "auf Dead City",
+        type: ActivityType.Playing,
+        statusbar: "online"
+    },
+    {
+        name: "!help | Dead City",
+        type: ActivityType.Playing,
+        statusbar: "online"
+    }
+];
+async function pickPresence() {
+    const option = Math.floor(Math.random() * statusArray.length);
+
+    try {
+        await client.user.setActivity({
+            name: statusArray[option].name,
+            type: statusArray[option].type,
+            status: statusArray[option].statusbar
+        })
+    } catch (error) {
+        console.error(error);
+    }
+}
 client.once("ready", () => {
     console.log("Bot ist online!")
-    client.user.setActivity({name: "auf Dead City", type: ActivityType.Playing})
 })
+setInterval(pickPresence, 30 * 1000);
 client.on('guildMemberAdd', (member) => {
     const message = "Hey <@" +member.user+ "> Herzlich willkommen auf **Dead City**! 🎉🤗\nGehe bitte zu <#940232290628419650> und bestätige sie bitte mit ✅.\nDu kannst dir außerdem in <#940232290628419652> eigene Rollen geben."
     const channel = member.guild.channels.cache.get(welcome_channel)
     channel.send(message)
     member.send("Hey <@" +member.user+ ">! Das ganze Team von **Dead City** wünscht dich noch einmal herzlich willkommen auf unserem Server und wir wünschen dir viel Spaß.\nFalls du es noch nicht gesehen hast, um unserem Server zu joinen, gehe bitte zu <#940232290628419650> und bestätige unsere Regeln bitte mit ✅.\nBei Fragen kannst du unter <#940232291018473491> ein Ticket erstellen.")
 })
-client.on("guildMemberRemove", (member) => {
-    const channel = member.guild.channels.cache.get(log_channel)
-    channel.send('Der User <@' +member.user+ '> hat den Server verlassen!');
-});
-
 
 
 
@@ -89,24 +109,40 @@ client.on("messageCreate", (message) => {
     const cmd = messageArray[0];
 
     
-    // Commands - (Prefix: !)
-if(command === 'test') {
-    message.channel.send("Test erfolgreich! **Prefix Commands** funktionieren!");
+// Commands - (Prefix: !)
+if(command === 'help') {
+    // sendet eine Hilfe Nachricht
+    const embed = new EmbedBuilder()
+        .setColor('DARK_BLUE')
+        .setTitle('Dead City Bot Hilfe')
+        .setURL('https://dead-city.grafkox.de')
+        .setAuthor({ name: 'Dead City', iconURL: client.user.displayAvatarURL(), url: 'https://dead-city.grafkox.de/bot/' })
+        .setThumbnail('https://dead-city.grafkox.de/assets/cut.png')
+        .addFields(
+            { name: 'Prefix', value: '*!*' },
+            { name: 'Commands', value: '\n!test\n!dm\n!message\n!clear\n!clearall\n!mc\n!help\n!inv\n' },
+        )
+        .setFooter({ text: 'Bot made by Grafkox_LP#7287', iconURL: 'https://cdn.discordapp.com/avatars/455285844350074881/d0b66b726036730c61206600c69c82e4.png?size=2048' });
+    message.channel.send({ embeds: [embed] });
 }
 
 if(command === 'dm') {
-    // sendet einem User eine DM (Format: !dm @User Nachricht)
-    const user = message.mentions.users.first();
-    if(user) {
-        const member = message.guild.members.cache.get(user.id);
-        if(member) {
-            member.send(argument.join(" "));
-            message.channel.send("Nachricht erfolgreich gesendet!");
+    if(message.member.roles.cache.some(role => role.name === '🔴 | Projektleitung')) {
+        // sendet einem User eine DM (Format: !dm @User Nachricht)
+        const user = message.mentions.users.first();
+        if(user) {
+            const member = message.guild.members.cache.get(user.id);
+            if(member) {
+                member.send(argument.join(" "));
+                message.channel.send("Nachricht erfolgreich gesendet!");
+            } else {
+                message.channel.send("Der User ist nicht auf diesem Server!");
+            }
         } else {
-            message.channel.send("Der User ist nicht auf diesem Server!");
+            message.channel.send("Du hast keinen User angegeben!");
         }
     } else {
-        message.channel.send("Du hast keinen User angegeben!");
+        message.channel.send("Du hast nicht die Berechtigung diesen Befehl zu nutzen!");
     }
 }
 
@@ -116,7 +152,6 @@ if(command === 'message') {
         const channel = message.mentions.channels.first();
         if(channel) {
             channel.send(argument.join(" ").slice(22));
-            message.channel.send("Nachricht erfolgreich gesendet!");
         } else {
             message.channel.send("Du hast keinen Channel angegeben!");
         }
@@ -126,7 +161,7 @@ if(command === 'message') {
 }
 
 if(command === 'clear') {
-    if(message.member.roles.cache.some(role => role.name === '🔴 | Projektleitung')) {
+    if(message.member.roles.cache.some(role => role.name === '🔵 | Moderator') || message.member.roles.cache.some(role => role.name === '🔴 | Admin')) {
         // löscht eine Nachricht (Format: !clear Anzahl)
         if(!args[0]) return message.reply("Bitte gebe eine Anzahl an!");
         message.channel.bulkDelete(args[0]);
@@ -136,7 +171,7 @@ if(command === 'clear') {
 }
 
 if(command === 'clearall'){
-    if(message.member.roles.cache.some(role => role.name === '🔴 | Projektleitung')) {
+    if(message.member.roles.cache.some(role => role.name === '🔵 | Moderator') || message.member.roles.cache.some(role => role.name === '🔴 | Admin')) {
         // löscht alle Nachrichten (Format: !clearall)
         message.channel.bulkDelete(100);
     } else {
@@ -181,29 +216,80 @@ if(command === 'mc') {
     });
 }
 
-if(command === 'help') {
-    // sendet eine Hilfe Nachricht
-    const embed = new EmbedBuilder()
-        .setColor('DARK_BLUE')
-        .setTitle('Dead City Bot Hilfe')
-        .setURL('https://dead-city.grafkox.de')
-        .setAuthor({ name: 'Dead City', iconURL: client.user.displayAvatarURL(), url: 'https://dead-city.grafkox.de/bot/' })
-        .setThumbnail('https://dead-city.grafkox.de/assets/cut.png')
-        .addFields(
-            { name: 'Prefix', value: '*!*' },
-            { name: 'Commands', value: '\n!test\n!dm\n!message\n!clear\n!clearall\n!mc\n!help\n!inv\n' },
-        )
-        .setFooter({ text: 'Bot made by Grafkox_LP#7287', iconURL: 'https://cdn.discordapp.com/avatars/455285844350074881/d0b66b726036730c61206600c69c82e4.png?size=2048' });
-    message.channel.send({ embeds: [embed] });
-}
-
 if(command === 'inv'){
     // delete command message
     message.delete();
-
     //create a new invite link and send it to the user via dm
     message.channel.createInvite({maxAge: 0, maxUses: 0}).then(invite => {
         message.author.send('Hier ist dein Invite Link:\nhttps://discord.gg/VEZQzXA73P');
     });
+}
+
+if(command === 'kick') {
+    if(message.member.roles.cache.some(role => role.name === '🔵 | Moderator') || message.member.roles.cache.some(role => role.name === '🔴 | Admin')) {
+        // kickt einen User (Format: !kick @User Grund)
+        let member = message.mentions.members.first();
+        if(!member)
+            return message.reply("Bitte gebe einen User an!");
+        if(!member.kickable)
+            return message.reply("Ich kann diesen User nicht kicken!");
+
+        let reason = args.slice(1).join(' ');
+        if(!reason) reason = "Kein Grund angegeben";
+
+        member.kick(reason)
+            .catch(error => message.reply(`Sorry ${message.author} ich konnte den User nicht kicken wegen: ${error}`));
+        const channel = member.guild.channels.cache.get(log_channel)
+        message.delete();
+        // sende eine embed Nachricht mit dem Grund
+        const embed = new EmbedBuilder()
+            .setColor('DARK_BLUE')
+            .setTitle('Dead City | Kick')
+            .setURL('https://dead-city.grafkox.de')
+            .setAuthor({ name: 'Dead City', iconURL: client.user.displayAvatarURL(), url: 'https://dead-city.grafkox.de/bot/' })
+            .setThumbnail('https://dead-city.grafkox.de/assets/cut.png')
+            .addFields(
+                { name: 'User', value: `${member.user.tag}\n` },
+                { name: 'Kick Grund', value: `${reason}` },
+            )
+            .setFooter({ text: 'Bot made by Grafkox_LP#7287', iconURL: 'https://cdn.discordapp.com/avatars/455285844350074881/d0b66b726036730c61206600c69c82e4.png?size=2048' });
+        channel.send({ embeds: [embed] });
+    } else {
+        message.channel.send("Du hast nicht die Berechtigung diesen Befehl zu nutzen!");
+    }
+}
+
+if(command === 'ban') {
+    if(message.member.roles.cache.some(role => role.name === '🔴 | Admin')) {
+        // bannt einen User (Format: !ban @User Grund Zeit in Tagen)
+        let member = message.mentions.members.first();
+        if(!member)
+            return message.reply("Bitte gebe einen User an!");
+        if(!member.bannable)
+            return message.reply("Ich kann diesen User nicht bannen!");
+
+        let reason = args.slice(1).join(' ');
+        if(!reason) reason = "Kein Grund angegeben";
+
+        member.ban({ reason: reason})
+            .catch(error => message.reply(`Sorry ${message.author} ich konnte den User nicht bannen wegen: ${error}`));
+        const channel = member.guild.channels.cache.get(log_channel)
+        message.delete();
+        // sende eine embed Nachricht mit dem Grund
+        const embed = new EmbedBuilder()
+            .setColor('DARK_BLUE')
+            .setTitle('Dead City | Ban')
+            .setURL('https://dead-city.grafkox.de')
+            .setAuthor({ name: 'Dead City', iconURL: client.user.displayAvatarURL(), url: 'https://dead-city.grafkox.de/bot/' })
+            .setThumbnail('https://dead-city.grafkox.de/assets/cut.png')
+            .addFields(
+                { name: 'User', value: `${member.user.tag}\n` },
+                { name: 'Ban Grund', value: `${reason}` },
+            )
+            .setFooter({ text: 'Bot made by Grafkox_LP#7287', iconURL: 'https://cdn.discordapp.com/avatars/455285844350074881/d0b66b726036730c61206600c69c82e4.png?size=2048' });
+        channel.send({ embeds: [embed] });
+    } else {
+        message.channel.send("Du hast nicht die Berechtigung diesen Befehl zu nutzen!");
+    }
 }
 })
