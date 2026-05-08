d0ceaac3b8adae7705e30591ab70d6e60c860b46
Leon Herschel <herschelleon@gmail.com>
2026-05-08 12:04:19 +0200

    updated file path

--- src/config.js
+++ src/config.js
@@ -15,7 +15,7 @@ module.exports = {
         color: '#206694',
         websiteUrl: 'https://pactmc.de',
         botPathUrl: 'https://pactmc.de/bot/',
-        thumbnailUrl: 'https://pactmc.de/assets/pactsmp-icon-i2-transparent.png',
+        thumbnailUrl: 'https://pactmc.de/assets/pactmc-icon-i2-transparent.png',
         authorName: 'PactMC',
         footerText: 'Bot made by @grafkox_lp',
         footerIconUrl: 'https://cdn.discordapp.com/avatars/455285844350074881/e799202175df16a29598e9e8969dfb52.png?size=2048',
